9185b4800359167edc193ccea3f72726300a3f0e
Waxter_ <58083410+xxxMclaren570Sxxx@users.noreply.github.com>
2023-05-19 08:24:16 -0400

    Add files via upload

--- src/Main/MainMenu.java
+++ src/Main/MainMenu.java
@@ -7,63 +7,56 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainMenu {
-	JFrame frame;
-	boolean hasWon = false;
-//	ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("res\\MainMenuBG.png"));
-//	Image img = icon.getImage();
-	public MainMenu(boolean DoSomt){
-		
-	}
-	public MainMenu() {
-		// Makes a main menu for the game
-		frame = new JFrame("Game Main Menu");
-		frame.setSize(500, 500);
-		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new BorderLayout());
-		frame.setLocationRelativeTo(null);
-		//frame.setIconImage(img);
+	 JFrame frame;
+	    boolean hasWon = false;
+	    public MainMenu(boolean doSmt){
+	    	
+	    }
 
-		JLabel titleLabel = new JLabel("Game Name");
-		titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
-		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		frame.add(titleLabel, BorderLayout.NORTH);
+	    public MainMenu() {
+	        // Makes a main menu for the game
+	        frame = new JFrame("Game Main Menu");
+	        frame.setSize(500, 500);
+	        frame.setResizable(false);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setLocationRelativeTo(null);
+	        ImageIcon icon = new ImageIcon("res\\MainMenuBG.jpg");
+	        
+	        JLabel background = new JLabel();
+	        background.setIcon(icon);
+	        background.setHorizontalAlignment(SwingConstants.CENTER);
+	        frame.add(background);
 
-		JPanel buttonPanel = new JPanel(new GridLayout(3, 1, 10, 10));
+	        JLabel titleLabel = new JLabel("Game Name");
+	        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+	        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+	        frame.add(titleLabel);
 
-		JButton startButton = new JButton("Start Game");
-		startButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// Code to start the game
-				Grid grid = new Grid();
-				grid.setVisible(true);
-				frame.setVisible(false);
-				frame.setLocationRelativeTo(null);
-				//JOptionPane.showMessageDialog(frame, "Starting the game...");
-				
-			}
-		});
-		JButton exitButton = new JButton("Exit");
-		exitButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// Code to exit the game
-				System.exit(0);
-			}
-		});
 
-		buttonPanel.add(startButton);
-		buttonPanel.add(exitButton);
+	        JButton startButton = new JButton("Start Game");
+	        startButton.setIcon(icon);
+	        startButton.setBounds(0, 0, 100, 100);
+	        startButton.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                // Code to start the game
+	                Grid grid = new Grid();
+	                grid.setVisible(true);
+	                frame.setLocationRelativeTo(null);
+	                //JOptionPane.showMessageDialog(frame, "Starting the game...");
+	            }
+	        });
 
-		frame.add(buttonPanel, BorderLayout.CENTER);
+	       
 
-		frame.setVisible(true);
-	}
-	
-	
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			MainMenu mainMenu = new MainMenu();
-			
-		});
-	}
+
+	        frame.add(startButton);
+
+	        frame.setVisible(true);
+	    }
+
+	    public static void main(String[] args) {
+	        SwingUtilities.invokeLater(() -> {
+	            MainMenu mainMenu = new MainMenu();
+	        });
+	    }
 }
